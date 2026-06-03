b49a0ecc065ddf9e970cb2a1bdceb2ea23adc29d
Paresh Solanki <solankiparesh1183@gmail.com>
2026-06-03 16:46:37 +0530

    refactor: remove OTP email and WhatsApp notification services from lead onboarding

--- routes/leads.js
+++ routes/leads.js
@@ -7,9 +7,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 const rateLimit = require('express-rate-limit');
 const { verifyToken } = require('./auth');
-const { sendOtpEmail } = require('../services/emailService');
 const { 
-  sendOtpOnWhatsapp, 
   normalizePhone, 
   buildManualWhatsAppMessage, 
   logWhatsAppActivity 
