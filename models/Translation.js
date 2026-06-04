a830fc52a60480e30b25fe32b923192ada0fef13
Paresh Solanki <solankiparesh1183@gmail.com>
2026-06-05 03:25:45 +0530

    feat: update Translation model to enforce unique constraint on key and lang fields

--- models/Translation.js
+++ models/Translation.js
@@ -4,18 +4,23 @@ const sequelize = require('../config/database');
 const Translation = sequelize.define('Translation', {
   key: {
     type: DataTypes.STRING,
-    allowNull: false,
-    unique: 'translation_unique_key'
+    allowNull: false
   },
   lang: {
     type: DataTypes.STRING,
-    allowNull: false,
-    unique: 'translation_unique_key'
+    allowNull: false
   },
   value: {
     type: DataTypes.TEXT,
     allowNull: false
   }
+}, {
+  indexes: [
+    {
+      unique: true,
+      fields: ['key', 'lang']
+    }
+  ]
 });
 
 module.exports = Translation;
